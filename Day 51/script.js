cff2c36afd2b67d4dc1867ff1944df883a84f873
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-03-25 16:29:19 +0400

    commit

---
+++ Day 51/script.js
@@ -0,0 +1,29 @@
+ const totalEgg = 6;
+
+ const nestsPerEgg = 2;
+
+ const birdsPerNest = 3;
+
+ const eggsPerNest = 3;
+
+ const totalNests = totalEgg * nestsPerEgg;
+
+ const totalBats = totalNests * eggsPerNest;
+
+console.log("Total number of bats that will hatch: " + totalBats);
+
+
+
+
+const myName = "Shio"
+
+const motherName = "Nato"
+
+
+
+
+ let myAge = 13
+
+ let motherAge= 50
+
+console.log(myAge + 20,motherAge)
