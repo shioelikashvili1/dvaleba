5bab4230fe125ea0ca31bb34c0579d29c5761058
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-04-04 16:53:54 +0400

    commit

---
+++ Day 53/script.js
@@ -0,0 +1,87 @@
+let x = 5;
+let y = x++;
+
+
+let x = 10;
+let y = x--;
+
+
+let i = 0;
+while (i < 5) {
+    console.log(i++); 
+}
+
+
+
+let i = 5;
+while (i > 0) {
+    console.log(i--);
+}
+
+
+let arr = [10, 20, 30];
+let index = 0;
+console.log(arr[index++]);
+
+
+
+let x = 5;
+let y = ++x;
+
+
+let x = 10;
+let y = --x;
+
+let i = 0;
+while (i < 5) {
+    console.log(++i);
+
+let i = 5;
+while (i > 0) {
+console.log(--i);
+
+
+let arr = [10, 20, 30];
+let index = 0;
+console.log(arr[++index]);
+
+
+5 == 5  
+'hello' == 'world'  
+5 == '5'   
+
+
+5 === 5  
+5 === '5'  
+
+5 != 10  
+'hello' != 'world' 
+ 
+
+
+5 !== '5'   
+5 !== 5   
+
+
+'abc' > 'def'  
+
+10 >= 10   
+'abc' >= 'abcd'  
+
+
+5 < 10  
+'abc' < 'def' 
+
+
+5 <= 5  
+'abc' <= 'abcd'  
+
+
+
+true && true   
+true && false   
+
+
+
+true || false  
+false || false   
