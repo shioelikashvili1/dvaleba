35bfbf5e2da81c96d7c337de25bed779b981d104
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-04-11 18:32:26 +0400

    commit

---
+++ Day 56/script.js
@@ -0,0 +1,56 @@
+// davaleba n1
+function Math(x,y) {
+    console.log(x+y)
+}
+
+Math(5,2)
+
+function Math(x,y) {
+    console.log(x+y)
+}
+
+Math(5,2)
+
+function Math(x,y) {
+    console.log(x+y)
+}
+
+Math(5,2)
+
+function Math(x,y) {
+    console.log(x+y)
+}
+
+Math(5,2)
+
+// davaleba n2
+
+function Triangle(height, width) {
+    console.log(height*width)
+}
+
+Triangle(5,10)
+
+// davaleba n3
+
+function GoaBest() {
+    console.log(GOABEST)
+}
+
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+GoaBest()
+
+// davleba n4
+
+function Perimetr(height, width){
+    console.log(height+height+width+width)
+}
+
+Perimetr(10, 20)
