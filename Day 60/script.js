6e6ef62613aa25ba54cfa95c2ceefde2b3074367
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-04-16 21:43:22 +0400

    commit

---
+++ Day 60/script.js
@@ -0,0 +1,65 @@
+//DAVALEBA 1
+
+console.log(20>12)
+console.log(12>32)
+console.log(542<687)
+console.log(234>988)
+console.log(543<987)
+console.log(543>8767)
+console.log(234<9875)
+console.log(564=8674)
+console.log(65=65)
+console.log(764=764)
+console.log(67543<98433)
+console.log(542<687)
+console.log(234>988)
+console.log(543<987)
+console.log(543>8767)
+console.log(234<9875)
+console.log(564=8674)
+console.log(65=65)
+console.log(764=764)
+console.log(6753<9843)
+
+
+//DAVALEBA 2
+
+
+console.log(764=764)
+console.log(67543<98433)
+console.log(542<687)
+console.log(234<988)
+console.log(543<987)
+console.log(543<8767)
+console.log(234<9875)
+console.log(564<8674)
+console.log(65=65)
+console.log(764=764)
+console.log(6753<9843)
+
+//DAVALEBA 3
+
+console.log(234>988)
+console.log(543>987)
+console.log(543>8767)
+console.log(234>9875)
+console.log(564=8674)
+console.log(621>69)
+console.log(764=74)
+console.log(67543>98433)
+console.log(67653<878)
+console.log(72=34)
+
+//DAVALEBA 4
+
+function shedareba(x, y){
+    console.log(x < y)
+}
+
+shedareba(10, 20)
+
+//DAVALEBA 5
+
+let x = prompt("give me a number: ")
+let y = prompt("give me a number: ")
+console.log(x < y)
