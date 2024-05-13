a0c4d50e69c2362dcb414b5ea73de8a4b86cee30
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-05-13 16:18:00 +0400

    commit

---
+++ Day 66/script.js
@@ -0,0 +1,128 @@
+
+
+
+for(let i = 1;i<10;i++){
+    console.log(i)
+}
+
+
+
+
+for(let x = 2;x<20;x+=2){
+    console.log(x)
+}
+
+
+let m = 5
+
+let numer = 0
+
+for(r = m;m<51;m+=5){
+
+    console.log('5*', numer+=1, '=', m)
+}
+
+
+
+
+
+
+
+
+let w = Number(prompt('enter an number'))
+for(let num = 0;num<w;num+=1){
+    if(num % 2 === 0){
+        alert('its even ')
+    }
+
+    else{
+        alert('its not evan')
+    }
+}
+
+
+
+
+
+
+for(let i = 100;i>0;i--){
+    console.log(i)
+}
+
+
+
+
+let GOA = 'ILOVEGOA'
+
+for(let n = 0;n < GOA.length;n++){
+    console.log(n)
+}
+
+
+
+
+
+let GOa = 'ILOVEGOA'
+
+for(let j = 0;j < GOa.length;j++){
+    console.log(GOa[j])
+}
+
+
+
+
+let i = 0
+
+while (i<10){
+    i = i+1
+    console.log(i)
+}
+
+console.log('HAPPY NEW YEAR')
+
+
+
+
+f = 1
+
+
+while (f<20){
+  f = f+2
+ console.log(f)
+
+}
+
+
+
+
+
+v = !6
+
+
+while (v<6){
+    v = v + 1
+    console.log(v)
+}
+
+
+
+
+let b = Number(prompt('enter an number between a,b'))
+
+
+for(let a = 0;a<b;a++){
+    alert(a)
+}
+
+
+
+// sixth homework
+
+
+m = 100
+
+
+while (m>0){
+
+    console.log(m--)
+}
