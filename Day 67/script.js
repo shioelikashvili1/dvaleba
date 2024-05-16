a7947029c1eff1a6d372e68cdb3463256a0ad492
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-05-16 18:22:03 +0400

    Commit

---
+++ Day 67/script.js
@@ -0,0 +1,4 @@
+document.getElementById("home").innerHTML = "Welcome to aversi!";
+document.getElementById("about").style.height = "50px";
+document.getElementById("products").style.width = "90px";
+document.getElementById("contact").style.backgroundColor = "red";
