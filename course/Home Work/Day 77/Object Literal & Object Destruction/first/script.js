f89b6f62d23f4ac4b2dc3716cadd4c8e680f331f
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-06-27 14:59:44 +0400

    commit

---
+++ course/Home Work/Day 77/Object Literal & Object Destruction/first/script.js
@@ -0,0 +1,16 @@
+let about = {
+    saxeli: prompt("შეიყვანეთ თქვენი სახელი"),
+    gvari: prompt("შეიყვანეთ თქვენი გვარი"),
+    asaki: prompt("შეიყვანეთ თქვენი ასაკი"),
+    qveyana: prompt("შეიყვანეთ თქვენი ქვეყყანა"),
+    qalaqi: prompt("შეიყვანეთ თქვენი ქალაქი"),
+    sayferi: prompt("შეიყვანეთ თქვენი საყვარელი ფერი"),
+    saysach: prompt("შეიყვანეთ თქვენი საყვარელი საჭმელი"),
+    saymanq: prompt("შეიყვანეთ თქვენი საყვარელი მანქანა"),
+    sayena: prompt("შეიყვანეთ თქვენი საყვარელი პროგრამირების ენა"),
+    sayakad: 'Goal-Oriented-Academy',
+
+}
+
+
+alert(`თქვენი სახელია ${about.saxeli}, თქვენი გვარია ${about.gvari}, თქვენი ასაკია ${about.asaki}, თქვენ ცხოვრობთ ${about.qveyana}-ში ${about.qalaqi}-ში, თქვენი საყვარელი ფერია ${about.sayferi},თქვენ გიყვართ ${about.saysach}, თქვენი საყვარელი მანქანაა ${abouT.saymanq}, თქვენი საყვარელი პროგრამირების ენაა ${about.sayena} და თქვენი საყვარელი აკადემიაა ${about.sayakad}`)
