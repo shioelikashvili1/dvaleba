8601a2f095f179b195851924e91c1fe1c780d2f8
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-07-11 00:00:28 +0400

    commit

---
+++ course/Project/calculatorr/script.js
@@ -0,0 +1,17 @@
+let display = document.getElementById('display');
+function appendToDisplay(input){
+ display.value += input;
+}
+
+function clearDisplay(){
+ display.value = "";
+}
+
+function calculate(){
+display.value = eval(display.value)
+}
+
+function Delete() {
+    let display = document.getElementById('display');
+    display.value = display.value.slice(0, -1);
+}
--- course/Project/number_generator/script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var button = document.getElementById("generateButton");
-    var display = document.getElementById("randomNumberDisplay");
-
-    button.addEventListener("click", function() {
-        var randomNumber = Math.floor(Math.random() * 100) + 1;
-        display.innerText = randomNumber;
-    });
-});
