7a5c0b5709d30d89dfac82171ef76ad60bf155f0
shioelikashvili1 <elikashvilishota59@gmail.com>
2024-11-16 19:24:06 +0400

    commit

--- Day 103/class work/script.js
+++
@@ -1,4 +0,0 @@
-const arr = ['3', '5', '8'];
-console.log(
-  arr.find(x => x == 8).repeat(2)
-);
---
+++ Day 103/class work/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+// App.jsx
+import React from 'react';
+import FormComponent from './FormComponent'; // Adjust the path if necessary
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Form Example</h1>
+      <FormComponent />
+    </div>
+  );
+}
+
+export default App;
+
